df37d6a8b7f899849a9fc2129f1bed1c904399ed
Franky2201 <77992031+Franky2201@users.noreply.github.com>
2024-02-11 17:49:54 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gde-win <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 13:39:41 by gde-win           #+#    #+#             */
-/*   Updated: 2024/02/10 17:47:15 by gde-win          ###   ########.fr       */
+/*   Updated: 2024/02/11 17:49:11 by gde-win          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,16 @@ void	ft_print_list(t_lexer_node *list)
 {
 	size_t	i;
 	char	quote[2];
+	char	separator[2];
 
 	quote[1] = '\0';
+	separator[1] = '\0';
 	i = 0;
 	while (list != NULL)
 	{
 		quote[0] = list->quote;
-		printf("[%zu]: $%s$\nquote: $%s$\n\n", i, list->str, list->quote ? quote : "NaN");
+		separator[0] = list->separator;
+		printf("[%zu]: $%s$\nquote: $%s$\nseparator: $%s$\n\n", i, list->str, list->quote ? quote : "NaN", list->separator ? separator : "NaN");
 		list = list->next;
 		i++;
 	}
@@ -120,7 +123,7 @@ t_lexer_node	*ft_word_segmentation(char *str)
 	}
 	return (list);
 }
-
+/*
 t_lexer_node	*ft_quote_segmentation(char *str)
 {
 	t_lexer_node	*list;
@@ -175,7 +178,7 @@ t_lexer_node	*ft_quote_segmentation(char *str)
 	}
 	return (list);
 }
-
+*/
 t_lexer_node	*ft_insert_node(t_lexer_node *list)
 {
 	t_lexer_node	*node;
@@ -234,47 +237,52 @@ t_lexer_node	*ft_token_segmentation(t_lexer_node *list)
 {
 	t_lexer_node	*current;
 	char			*original_str;
-	char			*temp;
 	size_t			i;
 	size_t			j;
 
 	current = list;
-	original_str = list->str;
 	while (current != NULL)
 	{
+		original_str = current->str;
+		current->closed_quote = true;
 		i = 0;
-		while (original_str[i] != '\0')
+		j = 0;
+		while (original_str[j] != '\0' && ((original_str[j] != '<' && original_str[j] != '>' && original_str[j] != '|') || current->closed_quote == false || original_str[j] == current->separator))
 		{
-			current->closed_quote = true;
-			j = 0;
-			while (original_str[i + j] != '\0' && ((original_str[i + j] != '<' && original_str[i + j] != '>' && original_str[i + j] != '|') || current->closed_quote == false))
-			{
-				if (original_str[i + j] == current->quote)
-					ft_switch_bool(&current->closed_quote);
-				if (current->closed_quote == true)
-				{
-					current->str = ft_substr(original_str, i, j - 1);
-					ft_insert_node(current);
-					current = current->next;
-					current->seperator = original_str[i + j];
-					i += j - 1;
-					j = 0;
-					while (original_str[i + j] == current->separator)
-						j++;
-					current->str = ft_substr(original_str, i, j - 1);
-					if (original_str[i + j] != '\0')
-					{
-						ft_insert_node(current);
-						current = current->next;
-					}
-				}
-				j++;
-			}
-			i += j;;
+			if (original_str[j] == current->quote)
+				ft_switch_bool(&current->closed_quote);
+			j++;
 		}
+		if (original_str[j] != '\0')
+		{
+			if (i != j)
+			{
+				current->str = ft_substr(original_str, i, j);
+				if (current->str == NULL)
+					return (ft_free_list(list));
+				ft_insert_node(current);
+				current->next->quote = current->quote;
+				current = current->next;
+			}
+			current->separator = original_str[j];
+			i = j;
+			while (original_str[j] == current->separator)
+				j++;
+			current->str = ft_substr(original_str, i, j - i);
+			if (original_str[j] != '\0')
+			{
+				ft_insert_node(current);
+				current->next->str = ft_strdup(original_str + j);
+				current->next->quote = current->quote;
+			}
+			free(original_str);
+		}
+		current = current->next;
 	}
+	return (list);
 }
 
+/*
 t_lexer_node	*ft_clean_tokens(t_lexer_node *list)
 {
 
@@ -287,7 +295,7 @@ t_lexer_node	*ft_clean_tokens(t_lexer_node *list)
 				return (ft_free_list(list));
 		}
 }
-
+*/
 int	main(int ac, char **av)
 {
 	t_lexer_node	*list;
@@ -309,13 +317,13 @@ int	main(int ac, char **av)
 		free(str);
 		return (EXIT_FAILURE);
 	}
-/*	list = ft_token_segmentation(list);
+	list = ft_token_segmentation(list);
 	if (list == NULL)
 	{
 		free(str);
 		return (EXIT_FAILURE);
 	}
-*/	//ft_print_list(list);
+	ft_print_list(list);
 	free(str);
 	ft_free_list(list);
 	return (EXIT_SUCCESS);
